40548a4d3bc0c3f55399e0071d61dc39258b59db
Marcus <marcus.vi.mar@gmail.com>
2026-03-01 18:15:22 -0300

    User entity and resource

---
+++ src/main/java/com/projetonelio/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.projetonelio.course.resources;
+
+import com.projetonelio.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Marcus", "marcus@gmail.com", "999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}

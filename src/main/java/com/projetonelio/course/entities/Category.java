b002b63469d2fd81c54d0b04ebf6adf2a3611ec7
Marcus <marcus.vi.mar@gmail.com>
2026-03-03 20:25:06 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/projetonelio/course/entities/Category.java
+++ src/main/java/com/projetonelio/course/entities/Category.java
@@ -1,5 +1,6 @@
 package com.projetonelio.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,6 +17,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {}
@@ -41,7 +44,6 @@ public class Category implements Serializable {
         this.name = name;
     }
 
-    @Transient
     public Set<Product> getProducts() {
         return products;
     }

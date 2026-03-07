3d586a218abe5406694380f3551d10ab3d74754f
Marcus <marcus.vi.mar@gmail.com>
2026-03-07 16:37:59 -0300

    Exception handling - findById

--- src/main/java/com/projetonelio/course/services/UserService.java
+++ src/main/java/com/projetonelio/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.projetonelio.course.services;
 
 import com.projetonelio.course.entities.User;
 import com.projetonelio.course.repositories.UserRepository;
+import com.projetonelio.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
